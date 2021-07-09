2f8f1707587554df3f6258d80a8e8a07d6c937ba
lucas.j <lucas.j@hpcnt.com>
2021-07-10 01:34:12 +0900

    페이징

--- app.js
+++ app.js
@@ -1,71 +1,104 @@
 import 'babel-polyfill';
-const API_URL = 'https://api.hnpwa.com/v0/news/1.json';
-const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-const CONTAINER = '#root';
 
 class NewsPage {
   #API_URL;
   #CONTENT_URL;
   #CONTAINER;
+  #currentPage;
+  #hasNext;
+  #items;
 
   constructor(apiUrl, contentUrl, container) {
     this.#API_URL = apiUrl;
     this.#CONTENT_URL = contentUrl;
     this.#CONTAINER = container;
+    this.#currentPage = 1;
+    this.#hasNext = true;
+    this.#items = [];
 
-    window.addEventListener('hashchange', this.#renderContent);
+    window.addEventListener('hashchange', this.render);
   }
 
+  #resetPage = () => {
+    document.querySelector(this.#CONTAINER).innerHTML = '';
+  };
+
   #getNews = async () => {
-    const response = await fetch(API_URL);
+    const response = await fetch(this.#API_URL.replace('@page', this.#currentPage));
     const json = await response.json();
     return json;
   };
 
   #getNew = async (id = 0) => {
-    const response = await fetch(CONTENT_URL.replace('@id', id));
+    const response = await fetch(this.#CONTENT_URL.replace('@id', id));
     const json = await response.json();
     return json;
   };
 
-  #renderContent = async () => {
-    const { title } = await this.#getNew(location.hash.slice(1));
-    const template = document.createElement('div');
-    template.innerHTML = `
-      <div>
-        <h1>${title}</h1>
-      </div>
-    `;
-    this.#append(CONTAINER, template.firstElementChild);
+  #renderContent = async id => {
+    const { title } = await this.#getNew(id);
+    this.#changePage(`
+    <div>
+      <h1>${title}</h1>
+      <a href="#/page/${this.#currentPage}">목록으로</a>
+    </div>
+    `);
   };
 
   #renderList = async () => {
-    const data = await this.#getNews();
-    const template = document.createElement('div');
-    template.innerHTML = `
-      <ul>
-        ${data
-          .map(({ id, title, comments_count }) => {
-            return `
-            <li>
-              <a href=#${id}>${title} (${comments_count})</a>
-            </li>
-          `;
-          })
-          .join('')}
-      </ul>
-    `;
-    this.#append(CONTAINER, template.firstElementChild);
+    this.#items = await this.#getNews();
+    this.#hasNext = this.#items.length === 30;
+
+    this.#changePage(`
+    <ul>
+      ${this.#items
+        .map(({ id, title, comments_count }) => {
+          return `
+          <li>
+            <a href="#/show/${id}">${title} (${comments_count})</a>
+          </li>
+        `;
+        })
+        .join('')}
+    </ul>
+    <div>
+        <a href="#/page/${Number(this.#currentPage) - 1 || 1}">이전 페이지</a>
+        <a href="#/page/${
+          this.#hasNext ? Number(this.#currentPage) + 1 : this.#currentPage
+        }">다음 페이지</a>
+    </div>
+    `);
   };
 
-  #append = (target, child) => {
-    document.querySelector(target).appendChild(child);
+  #changePage = htmlText => {
+    document.querySelector(this.#CONTAINER).innerHTML = htmlText;
   };
 
-  render() {
-    this.#renderList();
-  }
+  render = () => {
+    const { hash } = location;
+
+    const nameSpace = hash.match(/\/[a-z]+/)?.[0];
+
+    switch (nameSpace) {
+      case '/show': {
+        const id = hash.match(/\/[0-9]+/)?.[0].substring(1);
+        return this.#renderContent(id);
+      }
+      case '/page': {
+        this.#currentPage = hash.match(/#\/page\/(\d+)/)?.[1];
+        return this.#renderList();
+      }
+      default: {
+        this.#renderList();
+      }
+    }
+  };
 }
 
-const newsPage = new NewsPage();
-newsPage.render(API_URL, CONTENT_URL, CONTAINER);
+const newPage = new NewsPage(
+  'https://api.hnpwa.com/v0/news/@page.json',
+  'https://api.hnpwa.com/v0/item/@id.json',
+  '#root',
+);
+
+newPage.render();
